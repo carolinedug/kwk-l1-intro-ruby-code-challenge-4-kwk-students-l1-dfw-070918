05715720337b0a238fc853c20359b3588b3fe1e9
caroline dugan <caroline.dugan@gmail.com>
2018-07-12 14:29:49 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,5 +1,5 @@
 def least_coins(cents)
-  cost_hash = {:quarters => 0, :dimes => 0, :nickles => 0, :pennies => 0}
+  cost_hash = {:quarters => 0, :dimes => 0, :nickels => 0, :pennies => 0}
   while cents >= 25
   cost_hash[:quarters] += 1
   cents = cents - 25
@@ -9,7 +9,7 @@ def least_coins(cents)
   cents = cents - 10
   end
   while cents >= 5 
-  cost_hash[:nickles] += 1 
+  cost_hash[:nickels] += 1 
   cents = cents - 5
   end
   while cents >= 1
